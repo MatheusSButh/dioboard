ee9bee14557f45578218534d5430c7b7b638f15d
Matheus <matheusdsouzabuth@gmail.com>
2025-08-25 16:43:29 -0300

    First commit

---
+++ src/main/java/com/buthdev/demo/DioboardApplication.java
@@ -0,0 +1,13 @@
+package com.buthdev.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DioboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DioboardApplication.class, args);
+	}
+
+}
